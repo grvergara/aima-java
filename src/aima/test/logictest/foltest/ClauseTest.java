1316cd8614def006b74d0d66a0a23c6ef388ea93
Gonzalo Vergara <grvergara@gmail.com>
2024-10-29 23:34:53 -0300

    fix wrong encoded character

--- src/aima/test/logictest/foltest/ClauseTest.java
+++ src/aima/test/logictest/foltest/ClauseTest.java
@@ -572,7 +572,7 @@ public class ClauseTest extends TestCase {
 
 		FOLParser parser = new FOLParser(domain);
 
-		// p(x,y), q(a,b), ¬p(b,a), q(y,x)
+		//p(x,y),q(a,b),!p(b,a),q(y,x)
 		Clause c = new Clause();
 		c.addPositiveLiteral((Predicate) parser.parse("P(x,y)"));
 		c.addPositiveLiteral((Predicate) parser.parse("Q(A,B)"));
@@ -582,7 +582,7 @@ public class ClauseTest extends TestCase {
 		assertEquals("[[~P(B,A), P(B,A), Q(A,B)]]", c.getNonTrivialFactors()
 				.toString());
 		
-		// p(x,y), q(a,b), ¬p(b,a), ¬q(y,x)
+		// p(x,y), q(a,b),!p(b,a),!q(y,x)
 		c = new Clause();
 		c.addPositiveLiteral((Predicate) parser.parse("P(x,y)"));
 		c.addPositiveLiteral((Predicate) parser.parse("Q(A,B)"));
